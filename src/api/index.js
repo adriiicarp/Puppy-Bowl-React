fadba864b1d74deecda2d52342bfc909cc64361b
adriiicarp <adrianvarela@gmail.com>
2023-06-06 00:36:35 -0400

    rendered all the players

--- src/api/index.js
+++ src/api/index.js
@@ -1,46 +1,12 @@
 const COHORT = "2301-ftb-et-web-am";
-const baseUrl = `https://fsa-puppy-bowl.herokuapp.com/api/${COHORT}`;
+const baseUrl = `http://fsa-puppy-bowl.herokuapp.com/api/${COHORT}`
 
 export async function fetchAllPlayers() {
     try {
-        const response = await fetch(baseUrl);
-        const result = await response.json();
-        return result;
+      const response = await fetch(`${baseUrl}/players`);
+      const result = await response.json();
+      return result;    
     } catch (error) {
-        console.error(error);
-    }
-}
-
-export async function fetchSinglePlayer(id) {
-    try {
-        const response = await fetch(baseUrl);
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-export async function createPlayer(name, breed) {
-    try {
-        const response = await fetch(
-            'https://fsa-puppy-bowl.herokuapp.com/api/2301-ftb-et-web-am/players',
-            {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                name,
-                breed,
-              })
-            }
-          );
-   
-    const result = await response.json()
-    console.log("result: ", result)
-    return result;
-    } catch (error) {
-        console.error(error);
+      console.error(error);
     }
 }
--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { fetchAllPlayers } from "../api";
 
 export default function AllPlayers() {
-  return <div>AllPlayers</div>
+  const [players, setPlayers] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() =>{
+    async function getAllPlayers(){
+      const APIResponse = await fetchAllPlayers();
+        if (APIResponse.succes) {
+          setPlayers(APIResponse.data.players);
+        } else {
+          setError(APIResponse.error.message);
+        }
+    }
+    getAllPlayers();
+  }, []);
+  
+console.log("Players: ", players);
+console.log("Error in component", error);
+
+  return <div>AllPlayers</div>;
 }
+
